a60bcbc385c01c6cab9a6aac6ebc666ae8680d71
nguyenxuangiang22012004 <nguyenxuangiang22012004@gmail.com>
2025-11-06 15:31:59 +0700

    update

--- src/components/dashboard/DashboardSidebar.jsx
+++ src/components/dashboard/DashboardSidebar.jsx
@@ -2,21 +2,28 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const DashboardSidebar = ({ activePage }) => {
-
   const user = JSON.parse(localStorage.getItem("user")) || {};
   const userName = user.lastName || "User";
-   const linkItems = [
-    { to: "/dashboard/", label: "Manage My Account", key: "dashboard" },
-    { to: "/dashboard/dash-my-profile", label: "My Profile", key: "profile" },
+
+  const linkItems = [
+    { to: "/dashboard", label: "Manage My Account", key: "manage-account" },
+    { to: "/dashboard/dash-my-profile", label: "My Profile", key: "my-profile" },
+    { to: "/dashboard/dash-edit-profile", label: "Edit Profile", key: "edit-profile" },
     { to: "/dashboard/dash-address-book", label: "Address Book", key: "address-book" },
+    { to: "/dashboard/dash-address-add", label: "Add New Address", key: "address-add" },
+    { to: "/dashboard/dash-address-edit", label: "Edit Address", key: "address-edit" },
+    { to: "/dashboard/dash-address-default", label: "Set Default Address", key: "address-default" },
     { to: "/dashboard/dash-track-order", label: "Track Order", key: "track-order" },
-    { to: "/dashboard/dash-my-order", label: "My Orders", key: "my-orders" },
-    { to: "/dashboard/dash-payment-option", label: "My Payment Options", key: "payment-option" },
-    { to: "/dashboard/dash-cancellation", label: "My Returns & Cancellations", key: "cancellation" },
+    { to: "/dashboard/dash-my-order", label: "My Orders", key: "my-order" },
+    { to: "/dashboard/dash-manage-order", label: "Manage Orders", key: "manage-order" },
+    { to: "/dashboard/dash-payment-option", label: "Payment Options", key: "payment-option" },
+    { to: "/dashboard/dash-cancellation", label: "Returns & Cancellations", key: "cancellation" },
   ];
+
   return (
     <>
-       <div className="dash__box dash__box--bg-white dash__box--shadow u-s-m-b-30">
+      {/*====== Dashboard Sidebar ======*/}
+      <div className="dash__box dash__box--bg-white dash__box--shadow u-s-m-b-30">
         <div className="dash__pad-1">
           <span className="dash__text u-s-m-b-16">Hello, {userName}</span>
           <ul className="dash__f-list">
@@ -36,6 +43,8 @@ const DashboardSidebar = ({ activePage }) => {
           </ul>
         </div>
       </div>
+
+      {/*====== Dashboard Stats Box ======*/}
       <div className="dash__box dash__box--bg-white dash__box--shadow dash__box--w">
         <div className="dash__pad-1">
           <ul className="dash__w-list">
@@ -69,7 +78,7 @@ const DashboardSidebar = ({ activePage }) => {
           </ul>
         </div>
       </div>
-      {/*====== End - Dashboard Features ======*/}
+      {/*====== End - Dashboard Stats Box ======*/}
     </>
   );
 };
